3c5f7e887fbebe7a85dc919f38782299ae9ca72d
lliu11 <linl97@gmail.com>
2020-05-29 12:32:23 -0400

    delete files

--- Winter2020/inc/bmx_masterconfig.h
+++
@@ -1,6 +0,0 @@
-/// \file bmx_utilities.h
-/// \brief General purpose functions. These are useful functions that are not
-/// specifically dedicated to any one sensor or routine.
-
-/// \def [Macro Name]
-/// \brief description of the macro's purpose.
